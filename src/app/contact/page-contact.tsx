d44e8f840775bd151ab5e9beb4525206b749255c
Rainer Teixeira <raineroliveira94@hotmail.com>
2025-01-08 12:30:25 -0300

    feat: adicionar refatorar estrutura de pastas mover favicon e adicionar p ginas
    Foi implementada uma nova funcionalidade, melhorando a usabilidade.

---
+++ src/app/contact/page-contact.tsx
@@ -0,0 +1,10 @@
+// app/contact/page-contact.tsx
+export default function Contact() {
+    return (
+        <div className="max-w-screen-xl mx-auto p-8">
+            <h2 className="text-4xl font-semibold text-indigo-600 mb-4">Entre em Contato</h2>
+            <p className="text-lg text-gray-700 mb-8">Caso queira discutir projetos, parcerias ou oportunidades, entre em contato comigo pelo e-mail:</p>
+            <a href="mailto:raineroliveira94@hotmail.com" className="text-indigo-600 hover:underline">raineroliveira94@hotmail.com</a>
+        </div>
+    );
+}
