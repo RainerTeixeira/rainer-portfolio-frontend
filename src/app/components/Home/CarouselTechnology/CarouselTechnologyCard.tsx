b20d9ca959fc174ccce4988345aa3cff6d63c37c
Rainer Teixeira <raineroliveira94@hotmail.com>
2025-01-10 01:49:21 -0300

    Refatoração da interface: atualização de ícones, ajustes de cores e melhoria no estilo visual

--- src/app/components/Home/CarouselTechnology/CarouselTechnologyCard.tsx
+++ src/app/components/Home/CarouselTechnology/CarouselTechnologyCard.tsx
@@ -1,43 +1,40 @@
 import React from "react";
-import Image from "next/image";
-import { FaStar } from "react-icons/fa6";
 
-// Atualizando a interface para incluir 'description'
+// Constantes reutilizáveis
+const ICON_SIZE = 60;
+const CARD_PADDING = "p-6";
+const CARD_MARGIN = "m-2"; // Ajuste para margem menor
+const CARD_MAX_WIDTH = "max-w-[200px]"; // Reduzindo ainda mais a largura do card
+const CARD_MIN_HEIGHT = "min-h-[300px]";
+const CARD_TEXT_COLOR = "text-black text-opacity-80"; // Cor mais suave
+const DESCRIPTION_MARGIN_TOP = "mt-3";
+const HEADING_FONT_SIZE = "text-sm"; // Menor tamanho de fonte
+const HEADING_MARGIN_TOP = "mt-3"; // Espaçamento maior acima do título
+const HEADING_TEXT_COLOR = "text-black text-opacity-90"; // Cor do título
+const HEADING_ICON_COLOR = "text-indigo-600"; // Cor do ícone
+const TITLE_MARGIN_TOP = "mt-2"; // Ajuste para margem do título
+const ICON_MARGIN_BOTTOM = "mb-2"; // Ajuste para a margem do ícone
+
 interface Props {
   title: string;
-  image: string;
-  reviews: string;
-  description: string; // Nova propriedade para descrição
+  icon: React.ReactNode;
+  description: string;
 }
 
-const CarouselTechnologyCard: React.FC<Props> = ({ title, image, reviews, description }) => {
+const CarouselTechnologyCard: React.FC<Props> = ({ title, icon, description }) => {
   return (
-    <div className="bg-white p-6 rounded-lg m-3 max-w-[300px] w-full">
-      {/* Imagem */}
-      <div className="w-full h-[200px]">
-        <Image
-          src={image}
-          alt={title}
-          width={300}
-          height={200}
-          className="w-full h-full object-cover rounded-md"
-        />
+    <div
+      className={`bg-white ${CARD_PADDING} rounded-lg ${CARD_MARGIN} ${CARD_MAX_WIDTH} w-full shadow-md hover:shadow-lg hover:border-2 hover:border-indigo-300 transform transition-all duration-500 ease-in-out hover:scale-105 ${CARD_MIN_HEIGHT}`}
+    >
+      <div className="flex justify-center items-center flex-col">
+        <span className={`flex justify-center items-center ${ICON_MARGIN_BOTTOM} ${HEADING_ICON_COLOR}`}>
+          {React.cloneElement(icon, { size: ICON_SIZE })}
+        </span>
+        <h1 className={`${HEADING_MARGIN_TOP} ${HEADING_FONT_SIZE} font-semibold text-center ${HEADING_TEXT_COLOR}`}>
+          {title}
+        </h1>
+        <p className={`mt-3 text-sm text-justify ${CARD_TEXT_COLOR}`}>{description}</p>
       </div>
-      {/* Título */}
-      <h1 className="mt-5 text-xl text-black font-semibold">{title}</h1>
-      {/* Avaliações */}
-      <div className="flex items-center mt-2 space-x-3">
-        <div className="flex items-center">
-          {[...Array(5)].map((_, index) => (
-            <FaStar key={index} className="w-[1rem] h-[1rem] text-yellow-600" />
-          ))}
-        </div>
-        <div className="text-black opacity-80">({reviews})</div>
-      </div>
-      {/* Descrição */}
-      <p className="mt-2 text-black text-opacity-70 text-sm">
-        {description} {/* Exibe a descrição passada */}
-      </p>
     </div>
   );
 };
