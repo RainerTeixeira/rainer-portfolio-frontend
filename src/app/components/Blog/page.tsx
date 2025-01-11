01c459291f161fa79d50a339c30704f87de0d898
RainerTeixeira <raineroliveira94@hotmail.com>
2025-01-11 14:25:39 -0300

    chore: atualizar Ajustes no componente blog/página.tsx [c79baf2]
    Foram realizadas tarefas de manutenção e organização do projeto.

---
+++ src/app/components/Blog/page.tsx
@@ -0,0 +1,12 @@
+// src/app/components/Blog/page.tsx
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Blog Page</h1>
+        </div>
+    );
+};
+
+export default Page;  // Certifique-se de ter um export default ou named export
