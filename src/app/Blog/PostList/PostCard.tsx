c9e8795f492cc12e2dbcb1a8e20da0b1645f557b
RainerTeixeira <raineroliveira94@hotmail.com>
2025-01-14 01:18:01 -0300

    feat: modelagem e implementação de páginas dinâmicas do blog

---
+++ src/app/Blog/PostList/PostCard.tsx
@@ -0,0 +1,41 @@
+// src/app/Blog/Post/PostCard.tsx
+import React from "react";
+import { useRouter } from "next/navigation";
+
+type PostCardProps = {
+    title: string;
+    summary: string;
+    imageUrl?: string;
+    postId: string;
+};
+
+const PostCard: React.FC<PostCardProps> = ({ title, summary, imageUrl, postId }) => {
+    const router = useRouter();
+
+    const defaultImageUrl = "/images/f1.jpg";
+
+    const navigateToPost = () => {
+        router.push(`/Blog/Post/${postId}`);
+    };
+
+    return (
+        <div className="p-6 border rounded-lg shadow-lg hover:shadow-xl transition duration-300 transform hover:scale-105">
+            <img
+                src={imageUrl || defaultImageUrl}
+                alt={title}
+                className="w-full h-48 object-cover rounded-lg mb-4"
+                loading="lazy" // Para carregamento lento da imagem
+            />
+            <h3 className="text-xl font-semibold mb-2">{title}</h3>
+            <p className="text-gray-700 mb-4">{summary}</p>
+            <button
+                onClick={navigateToPost}
+                className="mt-4 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200"
+            >
+                Leia mais
+            </button>
+        </div>
+    );
+};
+
+export default PostCard;
