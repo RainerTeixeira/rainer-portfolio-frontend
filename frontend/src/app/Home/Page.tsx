7983f803c9c4a2c6cf1c0bb4d767250290a2214a
RainerTeixeira <raineroliveira94@hotmail.com>
2025-01-19 03:08:00 -0300

    chore: reorganizar a estrutura do projeto e atualizar os diretórios

---
+++ frontend/src/app/Home/Page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useEffect } from "react";
+import Carousel from "./Carousel/Carousel";
+import Technology from "./Technology/Technology";
+import CarouselTechnology from "./CarouselTechnology/CarouselTechnology";
+import Solutions from "./Solutions/Solutions";
+import Skills from "./Skills/Skills";
+import SupportRequest from "./SupportRequest/SupportRequest";
+import Newsletter from "./Newsletter/Newsletter";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const PortfolioHome = () => {
+  useEffect(() => {
+    // Carregar e inicializar AOS somente uma vez
+    AOS.init({
+      duration: 1000,
+      easing: "ease",
+      once: true,
+      anchorPlacement: "top-center",
+    });
+  }, []); // O array vazio faz a inicialização acontecer apenas uma vez
+
+  return (
+    <div>
+      <Carousel />
+      <Technology />
+      <CarouselTechnology />
+      <Solutions />
+      <Skills />
+      <SupportRequest />
+      <Newsletter />
+    </div>
+  );
+};
+
+export default PortfolioHome;
