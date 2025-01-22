ce35b8304a7cc7fc754a68a57f358aea9e5424ae
Rainer Teixeira <raineroliveira94@hotmail.com>
2025-01-22 00:52:25 -0300

    chore: estruturar projeto e atualizar diretórios
    - Reestruturado o projeto separando os diretórios `frontend` e `backend`.
    - Atualizadas as dependências e corrigidos erros de importação.
    - Removidos arquivos e configurações antigas, como o `next.config.js`.

---
+++ frontend/src/app/constant/constant.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        id: 1,
+        url: "/", // Referência para a página inicial
+        label: "home", // Texto exibido no link
+    },
+    {
+        id: 2,
+        url: "/sobre", // Referência para a página Sobre
+        label: "sobre", // Texto exibido no link
+    },
+    {
+        id: 3,
+        url: "/blog", // Referência para a página Blog
+        label: "blog", // Texto exibido no link
+    },
+    {
+        id: 4,
+        url: "/contato", // Referência para a página de Contato
+        label: "contato", // Texto exibido no link
+    },
+];
