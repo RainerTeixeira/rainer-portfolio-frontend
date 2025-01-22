d323dd158c6a783b2ff956466d29d7ca47a61617
Rainer Teixeira <raineroliveira94@hotmail.com>
2025-01-22 00:40:14 -0300

    Delete backend directory

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostsController } from './posts/posts.controller';
-
-@Module({
-    imports: [],
-    controllers: [PostsController],
-    providers: [],
-})
-export class AppModule { }
