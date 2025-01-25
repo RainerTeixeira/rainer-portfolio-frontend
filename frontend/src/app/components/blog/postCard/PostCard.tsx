de6e2ed09f8950be6004efd5e5c53269b1b1cd57
RainerTeixeira <raineroliveira94@hotmail.com>
2025-01-25 13:37:19 -0300

    feat: adicionar reestruturar backend para usar DynamoDB e adicionar configuração inicial [f36f164]
    Foi implementada uma nova funcionalidade, melhorando a usabilidade.

--- frontend/src/app/components/blog/postCard/PostCard.tsx
+++ frontend/src/app/components/blog/postCard/PostCard.tsx
@@ -14,15 +14,20 @@ const PostCard: React.FC<PostCardProps> = ({ title, summary, imageUrl, postId })
 
     const DEFAULT_IMAGE_URL = "/images/f1.jpg";
 
+    // Função para navegar para o post específico
     const navigateToPost = () => {
-        router.push(`/Blog/Post/${postId}`);
+        if (postId) {
+            router.push(`/Blog/Post/${postId}`);
+        } else {
+            console.error("ID do post não encontrado.");
+        }
     };
 
     return (
         <div className="p-6 border rounded-lg shadow-lg hover:shadow-xl transition duration-300 transform hover:scale-105">
             <div className="relative w-full h-48 mb-4">
                 <Image
-                    src={imageUrl || DEFAULT_IMAGE_URL}
+                    src={imageUrl || DEFAULT_IMAGE_URL} // Usa uma imagem padrão caso `imageUrl` seja indefinido
                     alt={title}
                     fill
                     className="object-cover rounded-lg"
@@ -31,7 +36,7 @@ const PostCard: React.FC<PostCardProps> = ({ title, summary, imageUrl, postId })
                 />
             </div>
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
-            <p className="text-gray-700 mb-4">{summary}</p>
+            <p className="text-gray-700 mb-4 line-clamp-3">{summary}</p> {/* Resumo limitado a 3 linhas */}
             <button
                 onClick={navigateToPost}
                 className="mt-4 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200"
